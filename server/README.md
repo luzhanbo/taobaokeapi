63c374cba1b543808c177097a49f04a83b3b3ddf
zhanbo.lu <luzhabno@163.com>
2019-12-08 21:03:11 +0800

    增加订单同步接口

---
+++ server/README.md
@@ -0,0 +1,10 @@
+# 文件介绍
++ api.js 淘宝客API的封装
++ config 淘宝客相关的配置文件 
++ clientApi.js 关端调用的接口
++ index.js next.js生成的express服务入口
+
+# api.js
++ 全网淘客商品搜索
++ 订单同步 
++ 商品详情及相似商品列表
--- server/clientApi.js
+++ server/clientApi.js
@@ -1,19 +1,5 @@
 const api = require('./api')
 
-const get_client_ip = function (req) {
-    var ipStr = req.headers['x-forwarded-for'] ||
-        req.ip ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress || '';
-    var ipReg = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
-    if (ipStr.split(',').length > 0) {
-        ipStr = ipStr.split(',')[0]
-    }
-    var ip = ipReg.exec(ipStr);
-    return ip[0];
-};
-
 const main = (app)=>{
     app.post('/api/test',(req,res)=>{
         console.log(req.body)
